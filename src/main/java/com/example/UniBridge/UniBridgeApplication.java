26f5e18cdfa8a673bbb404184cad09b0a8b1f288
Jiseong <ydgeoubooke@gmail.com>
2026-05-06 13:07:02 +0900

    BASE BUILD
    Spring Initialised.

---
+++ src/main/java/com/example/UniBridge/UniBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.UniBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniBridgeApplication.class, args);
+	}
+
+}

a558ec137636c097736ce5d460818f5371cf4d96
Jiseong <ydgeoubooke@gmail.com>
2026-05-06 13:39:27 +0900

    +MainController.java +mapping to main.html

--- src/main/java/com/example/UniBridge/UniBridgeApplication.java
+++ src/main/java/com/example/UniBridge/UniBridgeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UniBridgeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UniBridgeApplication.class, args);
 	}
-
 }

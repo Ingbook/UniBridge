132dde57bc0d685461e7dd74bddcb33a7ee17e9e
Jaemin-Gang <gangj0289@gmail.com>
2026-05-11 13:30:08 +0900

    test : 커밋 및 푸시

--- src/main/java/com/example/UniBridge/controller/MainController.java
+++ src/main/java/com/example/UniBridge/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.UniBridge.controller;
+package com.example.UniBridge;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;

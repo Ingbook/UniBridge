53806b5a4cce92be4502b0b8244ebe7892721d72
Jaemin-Gang <gangj0289@gmail.com>
2026-05-18 16:49:10 +0900

    feat :

--- src/main/java/com/example/UniBridge/UniBridgeApplication.java
+++ src/main/java/com/example/UniBridge/UniBridgeApplication.java
@@ -2,7 +2,7 @@ package com.example.UniBridge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//comit
 @SpringBootApplication
 public class UniBridgeApplication {
 	public static void main(String[] args) {

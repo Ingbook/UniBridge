38ce5f6414214229184c2c43be41c5c7ab0dc976
yonggyu <yonggyu1234@naver.com>
2026-05-18 20:59:21 +0900

    Analysis 컨트롤러 및 기능 구현

---
+++ src/main/java/com/example/UniBridge/analysis/GapAnalysisRequest.java
@@ -0,0 +1,11 @@
+package com.example.UniBridge.analysis;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GapAnalysisRequest {
+
+    private Long companyId;
+}
